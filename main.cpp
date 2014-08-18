18547ed85be815eebe0c2d570db4b78d9939a70d
edwardsCam <cedwards3054@gmail.com>
2014-08-18 09:34:16 -0700

    stage

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
+#include <iostream>
+#include <Windows.h>
 
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lparam)
+{
+    LPTSTR title = 0;
 
-int main() {
+        GetWindowText(hwnd, title, sizeof(title));
+        std::cout << "Window Name: " << std::endl;
 
+        return true;
+}
+
+int main(int argc, char* argv[]) // SUBSYSTEM:CONSOLE
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prev, LPSTR lpCmdLine, int nCmdShow) // SUBSYSTEM:WINDOWS
+{
+        EnumWindows(EnumWindowsProc, NULL);
+
+        std::cin.get();
+        return 0;
 }
