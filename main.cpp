4aaabd461e6b0693024c08ea97a8d30ee25dbb57
edwardsCam <cedwards3054@gmail.com>
2014-08-19 14:18:35 -0700

    SFML window fundamentals

--- main.cpp
+++ main.cpp
@@ -4,96 +4,71 @@
 #include <stdlib.h>
 #include <string.h>
 #include <tchar.h>
+#include "SFML\Window.hpp"
 #include "SFML\Graphics.hpp"
 
 // Global variables
 
-// The main window class name.
-static TCHAR szWindowClass[] = _T("win32app");
+unsigned int _width, _height;
 
-// The string that appears in the application's title bar.
-static TCHAR szTitle[] = _T("Planet Launcher");
+// Definitions
 
-HINSTANCE hInst;
+#define MAX_FRAMERATE 24
 
 // Forward declarations of functions included in this code module:
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
-	WNDCLASSEX wcex;
+	_width = 800;
+	_height = 600;
 
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style          = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc    = WndProc;
-	wcex.cbClsExtra     = 0;
-	wcex.cbWndExtra     = 0;
-	wcex.hInstance      = hInstance;
-	wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-	wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName   = NULL;
-	wcex.lpszClassName  = szWindowClass;
-	wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	sf::Window window;
+	window.create(sf::VideoMode(_width, _height), "Planet Launcher");
+	window.setFramerateLimit(MAX_FRAMERATE);
 
-	if (!RegisterClassEx(&wcex))
-	{
-		MessageBox(NULL,
-			_T("Call to RegisterClassEx failed!"),
-			_T("Win32 Guided Tour"),
-			NULL);
+	//game loop
+	while (window.isOpen())
+	{	
+		sf::Event event;
+		while (window.pollEvent(event)) {
 
-		return 1;
+			switch (event.type)
+			{
+			case (sf::Event::Closed):
+				{
+					//todo save state? or other things before closing
+					window.close();
+					break;
+				}
+
+			case (sf::Event::Resized):
+				{
+					sf::Vector2u size = window.getSize();
+					_width = size.x;
+					_height = size.y;
+					break;
+				}
+
+			case (sf::Event::KeyPressed):
+				{
+					//todo
+					break;
+				}
+
+			case (sf::Event::MouseButtonPressed):
+				{
+					//todo
+					break;
+				}
+
+				//todo: other events
+
+			}
+		}
 	}
 
-	hInst = hInstance; // Store instance handle in our global variable
-
-    // The parameters to CreateWindow explained:
-    // szWindowClass: the name of the application
-    // szTitle: the text that appears in the title bar
-    // WS_OVERLAPPEDWINDOW: the type of window to create
-    // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
-    // 500, 100: initial size (width, length)
-    // NULL: the parent of this window
-    // NULL: this application does not have a menu bar
-    // hInstance: the first parameter from WinMain
-    // NULL: not used in this application
-	HWND hWnd = CreateWindow(
-		szWindowClass,
-		szTitle,
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		500, 100,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-	);
-
-	if (!hWnd)
-	{
-		MessageBox(NULL,
-			_T("Call to CreateWindow failed!"),
-			_T("Win32 Guided Tour"),
-			NULL);
-
-		return 1;
-	}
-
-    // The parameters to ShowWindow explained:
-    // hWnd: the value returned from CreateWindow
-    // nCmdShow: the fourth parameter from WinMain
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
-
-    // Main message loop:
-	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return (int) msg.wParam;
+	return 0;
 }
 
 //
@@ -116,9 +91,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 
-        // Here your application is laid out.
-        // For this introduction, we just print out "Hello, World!"
-        // in the top left corner.
+		// Here your application is laid out.
+		// For this introduction, we just print out "Hello, World!"
+		// in the top left corner.
 		TextOut(hdc,
 			5, 5,
 			greeting, _tcslen(greeting));
